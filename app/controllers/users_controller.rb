a8c87c491f27f71b7d2920800dc517c474a14a16
SamvitaKarkal <samvitaskarkal@gmail.com>
2021-06-15 21:21:47 +0530

    Fixing Heroku push error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    users = User.all.as_json(only: %i[id first_name role])
+    users = User.all.as_json(only: %i[id first_name])
     render status: :ok, json: { users: users }
   end
 
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :role)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
   
 end
