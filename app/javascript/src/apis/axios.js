f84583e339be06a3bf3a7402145d486bdff23f70
SamvitaKarkal <samvitaskarkal@gmail.com>
2021-06-15 10:52:30 +0530

    Added role and its tests to user

--- app/javascript/src/apis/axios.js
+++ app/javascript/src/apis/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import Toastr from "components/Common/Toastr";
+//import Toastr from "components/Common/Toastr";
 //import { setToLocalStorage } from "helpers/storage";
 
 axios.defaults.headers = {
@@ -22,35 +22,35 @@ axios.defaults.headers = {
 //   setLoading(false);
 // };
 
-const handleSuccessResponse = response => {
-  if (response) {
-    response.success = response.status === 200;
-    if (response.data.notice) {
-      Toastr.success(response.data.notice);
-    }
-  }
-  return response;
-};
+// const handleSuccessResponse = response => {
+//   if (response) {
+//     response.success = response.status === 200;
+//     // if (response.data.notice) {
+//     //   Toastr.success(response.data.notice);
+//     // }
+//   }
+//   return response;
+// };
 
-const handleErrorResponse = error => {
-  if (error.response?.status === 401) {
-    setToLocalStorage({ email: null, userId: null, userName: null });
-  }
-  Toastr.error(
-    error.response?.data?.error ||
-      error.response?.data?.notice ||
-      error.message ||
-      error.notice ||
-      "Something went wrong!"
-  );
-  if (error.response?.status === 423) {
-    window.location.href = "/";
-  }
-  return Promise.reject(error);
-};
+// const handleErrorResponse = error => {
+//   if (error.response?.status === 401) {
+//     setToLocalStorage({ email: null, userId: null, userName: null });
+//   }
+//   Toastr.error(
+//     error.response?.data?.error ||
+//       error.response?.data?.notice ||
+//       error.message ||
+//       error.notice ||
+//       "Something went wrong!"
+//   );
+//   if (error.response?.status === 423) {
+//     window.location.href = "/";
+//   }
+//   return Promise.reject(error);
+// };
 
-export const registerIntercepts = () => {
-  axios.interceptors.response.use(handleSuccessResponse, error =>
-    handleErrorResponse(error)
-  );
-};
+// export const registerIntercepts = () => {
+//   axios.interceptors.response.use(handleSuccessResponse, error =>
+//     handleErrorResponse(error)
+//   );
+// };
